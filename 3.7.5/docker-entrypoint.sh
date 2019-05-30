e89e0c7fa6a2d466f48be747c6a4235e7fc525e7
Dimitar Bolinovski <d.bolinovski@voxteneo.com.mk>
2019-05-30 21:05:53 +0200

    Custom changes

--- 3.7.5/docker-entrypoint.sh
+++ 3.7.5/docker-entrypoint.sh
@@ -2,19 +2,4 @@
 
 set -e
 
-if [ ! -f /pgpoolII/pcp.conf ]; then
-    cp /etc/pcp.conf.sample /pgpoolII/pcp.conf
-fi
-if [ ! -f /pgpoolII/pgpool.conf ]; then
-    cp /etc/pgpool.conf.sample /pgpoolII/pgpool.conf
-fi
-if [ ! -f /pgpoolII/pool_hba.conf ]; then
-    cp /etc/pool_hba.conf.sample /pgpoolII/pool_hba.conf
-fi
-
-/usr/bin/pg_md5 -m -f /pgpoolII/pgpool.conf -u $PG_USERNAME $PG_PASSWORD
-# if [ "$1" = 'pgpool-server' ]; then
-# 	exec pgpool "$@"
-# fi
-
 exec "$@"
