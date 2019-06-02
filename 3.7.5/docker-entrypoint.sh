963d12340daba7f7b0a3d75d8bdf6018a0837e37
Dimitar Bolinovski <d.bolinovski@voxteneo.com.mk>
2019-06-02 12:36:32 +0200

    Generate pcp.conf username/password

--- 3.7.5/docker-entrypoint.sh
+++ 3.7.5/docker-entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+echo "$PG_USERNAME:`/usr/bin/pg_md5 ${PG_PASSWORD}`" > /pgpoolII/config/pcp.conf
 /usr/bin/pg_md5 -m -f /pgpoolII/config/pgpool.conf -u ${PG_USERNAME} ${PG_PASSWORD}
 
 exec "$@"
