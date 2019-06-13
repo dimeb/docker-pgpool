0c482f4b0024bba1a3d8f4672795123914662d26
Dimitar Bolinovski <d.bolinovski@voxteneo.com.mk>
2019-06-13 14:33:37 +0200

    Config files setup changes

--- 3.7.5/docker-entrypoint.sh
+++ 3.7.5/docker-entrypoint.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+if [ ! -f "/pgpoolII/config/pcp.conf" ]; then
+  cp /etc/pcp.conf.sample /pgpoolII/config/pcp.conf
+fi
+if [ ! -f "/pgpoolII/config/pool_hba.conf" ]; then
+  cp /etc/pool_hba.conf.sample /pgpoolII/config/pool_hba.conf
+fi
+if [ ! -f "/pgpoolII/config/pgpool.conf" ]; then
+  cp /etc/pgpool.conf.sample /pgpoolII/config/pgpool.conf
+fi
+
 fname=/pgpoolII/config/pcp.conf
 f=`cat $fname`
 cat < dev/null > $fname
@@ -10,8 +20,8 @@ for l in $f; do
     echo $l > $fname
   fi
 done
-echo "$PG_USERNAME:`/usr/bin/pg_md5 ${PG_PASSWORD}`" > $fname
+echo "$PG_USERNAME:`/usr/bin/pg_md5 $PG_PASSWORD`" > $fname
 
-/usr/bin/pg_md5 -m -f /pgpoolII/config/pgpool.conf -u ${PG_USERNAME} ${PG_PASSWORD}
+/usr/bin/pg_md5 -m -f /pgpoolII/config/pgpool.conf -u $PG_USERNAME $PG_PASSWORD
 
 exec "$@" > /pgpoolII/logs/pgpool.log 2>&1
