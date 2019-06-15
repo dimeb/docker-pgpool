8199e3adea6fcbd6b0693f3d4eefb378a9dd7993
Dimitar Bolinovski <d.bolinovski@voxteneo.com.mk>
2019-06-15 11:43:55 +0200

    Config files setup changes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,18 +2,7 @@
 
 set -e
 
-IFS="
-"
-
-fname=/pgpoolII/config/pcp.conf
-f=`cat $fname`
-cat < dev/null > $fname
-for l in $f; do
-  if [ "$PG_USERNAME" != "`echo -n $l | cut -c1-${#PG_USERNAME}`" ]; then
-    echo $l > $fname
-  fi
-done
-echo "$PG_USERNAME:`/usr/bin/pg_md5 $PG_PASSWORD`" > $fname
+echo "$PG_USERNAME:`/usr/bin/pg_md5 $PG_PASSWORD`" >> /pgpoolII/config/pcp.conf
 
 /usr/bin/pg_md5 -m -f /pgpoolII/config/pgpool.conf -u $PG_USERNAME $PG_PASSWORD
 
